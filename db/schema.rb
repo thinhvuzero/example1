98e8495fd7d57be765d92b22f452ce3035412d10
Vu.Duc.Thinh <vu.duc.thinh@framgia.com>
2018-08-02 09:21:26 +0700

    config mailler

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_08_02_015854) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
